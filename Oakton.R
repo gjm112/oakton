fa35c820737fde45d79b3095f46c1d031484354a
Gregoery J. Matthews <gregorymatthews@Gregorys-MacBook-Pro.local>
2026-02-27 10:44:29 -0600

    afhads

--- Oakton.R
+++ Oakton.R
@@ -88,9 +88,34 @@ Teams %>%
   summarize(W = sum(W), L = sum(L), n = n()) %>% 
   mutate(winpct = W/(W+L)) 
 
+
+#Questions
+#Batting: Find the player who had the lowest batting average in a season where they hit over 40HR. 
+Batting %>% group_by(playerID, yearID) %>%
+  summarize(HR = sum(HR), H = sum(H), AB = sum(AB)) %>%
+  filter(HR >= 40) %>% mutate(BA = H/AB) %>% arrange(BA) %>% head()
+
+#Pitching: What pitcher had the lowest HR rate (i.e. HR/IPouts) among all pitchers who pitched at least 100 innings in a season?
+
+#Advanced: You'll need to merge! Find the 
+
 ##########################################
 ## Data Visualization
 ##########################################
+Teams %>% filter(franchID %in% c("CHC","CHW") & yearID >= 2000) %>% ggplot(aes(x = W)) + geom_histogram() + theme_bw()
+Teams %>% filter(franchID %in% c("CHC","CHW") & yearID >= 2000) %>% ggplot(aes(x = W)) + geom_density() + theme_bw()
+Teams %>% filter(franchID %in% c("CHC","CHW") & yearID >= 2000) %>% ggplot(aes(y = W)) + geom_boxplot() + theme_bw()
+Teams %>% filter(franchID %in% c("CHC","CHW") & yearID >= 2000) %>% ggplot(aes(x = franchID, y = W, color = franchID)) + geom_boxplot() + theme_bw()
+
+#Scatter/line plots
+Teams %>% filter(franchID %in% c("CHC","CHW")) 
+Teams %>% filter(franchID %in% c("CHC","CHW") & yearID >= 2000) %>% ggplot(aes(x = yearID, y = W, color = franchID)) + geom_point() + geom_line() + scale_color_manual(values = c("CHC" = "blue", "CHW" = "black")) + theme_bw()
+
+Teams %>% filter(franchID %in% c("CHC","CHW") & yearID >= 2000) %>% 
+  ggplot(aes(x = yearID, y = W)) + 
+  facet_grid(~franchID) + 
+  geom_point() +
+  geom_line() 
 
 
 
